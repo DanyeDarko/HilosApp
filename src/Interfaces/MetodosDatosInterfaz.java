17decf1de53db2a01148733f7a74c9f94347a0fb
Danye <danielbrzen@outlook.com>
2020-01-19 22:59:55 -0600

    INTRFAZ CON METODOS ABSTRACTOS PARA LA CLASE DE TIPO DATO

---
+++ src/Interfaces/MetodosDatosInterfaz.java
@@ -0,0 +1,7 @@
+
+package Interfaces;
+
+public interface MetodosDatosInterfaz { 
+    int[][] generarNumeros(); // METODO ABSTRACTO PARA GENERAR NUMEROS ALEATORIOS EN EL ARREGLO 
+    void decidirOperacion(int numeroHilo,int inicio,int fin); // DECIDIMOS QUE OPERACION REALISAR CON SU RESPECTIVO RANGO OPERACIONAL
+}
