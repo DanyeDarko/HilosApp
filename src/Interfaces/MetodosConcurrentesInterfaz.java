adf2136a63249de0c56c80520b9ee8862ce86bfd
Danye <danielbrzen@outlook.com>
2020-01-19 23:00:53 -0600

    INTERFACES DE CLASES,HILOS  Y OBJETOS

--- src/Interfaces/MetodosConcurrentesInterfaz.java
+++
@@ -1,7 +0,0 @@
-
-package Interfaces;
-
-public interface MetodosConcurrentesInterfaz { 
-    int[][] generarNumeros(); // METODO ABSTRACTO PARA GENERAR NUMEROS ALEATORIOS EN EL ARREGLO 
-    void decidirOperacion(int numeroHilo,int inicio,int fin); // DECIDIMOS QUE OPERACION REALISAR CON SU RESPECTIVO RANGO OPERACIONAL
-}
